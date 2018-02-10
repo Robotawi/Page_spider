a7a66e11ecfc48d8365090acf3a13f13943663b6
Raessa <mohamed.s.raessa@gmail.com>
2018-02-10 23:42:03 +0900

    initial commit

---
+++ utilities/database_utilities.py
@@ -0,0 +1,5 @@
+def create_database(database_path: str):
+    #TODO: generate the database
+
+def save_words_to_database(database_path: str, words_list: list)
+    #TODO: save the words to the database
